d16ad7a6556ac09778ee3f4517b76d5017ecbda8
Matt WR <176295307+wr-aer@users.noreply.github.com>
2025-04-04 15:19:28 +0100

    build(AERT-199): implemented sqs queue that subscribes to sns topic

--- terraform/interpreter.tf
+++ terraform/interpreter.tf
@@ -35,3 +35,56 @@ data "aws_iam_policy_document" "email_received_topic_policy" {
     }
   }
 }
+
+resource "aws_sqs_queue" "email_received_queue" {
+  name = "${var.project_name}-email-received-queue"
+
+  redrive_policy = jsonencode({
+    maxReceiveCount     = 4
+    deadLetterTargetArn = aws_sqs_queue.email_received_deadletter_queue.arn
+  })
+}
+
+resource "aws_sqs_queue" "email_received_deadletter_queue" {
+  name = "${var.project_name}-email-received-deadletter-queue"
+}
+
+resource "aws_sqs_queue_redrive_allow_policy" "email_received_queue_redrive_allow_policy" {
+  queue_url = aws_sqs_queue.email_received_deadletter_queue.id
+
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = [aws_sqs_queue.email_received_queue.arn]
+  })
+}
+
+resource "aws_sns_topic_subscription" "email_received_topic_subscription" {
+  topic_arn = aws_sns_topic.email_received_topic.arn  
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.email_received_queue.arn 
+}
+
+resource "aws_sqs_queue_policy" "email_received_topic_subscription_policy_attachment" {
+  queue_url = aws_sqs_queue.email_received_queue.id
+  policy    = data.aws_iam_policy_document.email_received_topic_subscription_policy.json
+}
+
+data "aws_iam_policy_document" "email_received_topic_subscription_policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["sns.amazonaws.com"]
+    }
+
+    actions   = ["SQS:SendMessage"]
+    resources = [aws_sqs_queue.email_received_queue.arn]
+
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = [aws_sns_topic.email_received_topic.arn]
+    }
+  }
+}
